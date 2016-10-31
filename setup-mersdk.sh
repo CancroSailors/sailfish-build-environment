2628485dcaeac2b6400a0513cbd3051ccf027d44
Dinesh Manajipet <saidinesh5@gmail.com>
2016-10-31 18:53:15 +0530

    Fixing broken stuff

--- setup-mersdk.sh
+++ setup-mersdk.sh
@@ -5,6 +5,9 @@ SCRIPTPATH=`pwd`
 popd > /dev/null
 
 #Setup environment
+rm ~/.hadk.env
+rm ~/.mersdk.profile
+rm ~/.mersdkubu.profile
 ln -s $SCRIPTPATH/.hadk.env ~/.hadk.env
 ln -s $SCRIPTPATH/.mersdk.profile ~/.mersdk.profile
 ln -s $SCRIPTPATH/.mersdkubu.profile ~/.mersdkubu.profile
@@ -18,19 +21,24 @@ cp $SCRIPTPATH/cancro_local_manifest.xml $ANDROID_ROOT/.repo/local_manifests/can
 #Download Setup MER SDK
 cd $MER_TMPDIR
 TARBALL=mer-i486-latest-sdk-rolling-chroot-armv7hl-sb2.tar.bz2
-curl -k -O https://img.merproject.org/images/mer-sdk/$TARBALL
-mkdir -p $MER_ROOT/sdks/sdk
-sudo tar --numeric-owner -p -xjf $MER_TMPDIR/$TARBALL
-echo "export MER_ROOT=$MER_ROOT" >> ~/.bashrc
-echo 'alias mersdk=$MER_ROOT/sdks/sdk/mer-sdk-chroot' >> ~/.bashrc
-exec bash
-echo 'PS1="MerSDK $PS1"' >> ~/.mersdk.profile
-cd $HOME
-mersdk
-# These commands are a tmp workaround of glitch when working with target:
-sudo zypper ar \
-http://repo.merproject.org/obs/home:/sledge:/mer/latest_i486/ curlfix
-sudo zypper ref curlfix
-sudo zypper dup --from curlfix
+curl -k -O -C - https://img.merproject.org/images/mer-sdk/$TARBALL
 
-echo "Mer SDK setup. You can start MerSDK by simply typing mersdk on your bash shell. Good Luck!"
+SDK_ROOT=$MER_ROOT/sdks/sdk
+sudo rm -rf $SDK_ROOT
+mkdir -p $SDK_ROOT
+cd $SDK_ROOT
+sudo tar --numeric-owner -p -xjf $MER_TMPDIR/$TARBALL
+
+#Setup convenience bash aliases
+echo "export MER_ROOT=$MER_ROOT" >> ~/.bashrc
+echo "alias mersdk=$MER_ROOT/sdks/sdk/mer-sdk-chroot" >> ~/.bashrc
+
+cd $HOME
+
+# These commands are a tmp workaround of glitch when working with target:
+sudo chroot $SDK_ROOT sudo zypper ar http://repo.merproject.org/obs/home:/sledge:/mer/latest_i486/ curlfix
+sudo chroot $SDK_ROOT sudo zypper ref curlfix
+sudo chroot $SDK_ROOT sudo zypper dup --from curlfix
+
+echo "Mer SDK setup complete. You can start MerSDK by simply typing mersdk on your bash shell. Good Luck!"
+exec bash
